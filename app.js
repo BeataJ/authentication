cc47419128c116c0b3e3d3ae3d902aefd2571b47
beataj <beata@jasinski.com>
2023-09-02 15:51:28 -0700

    session

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const path = require('path');
 
 const express = require('express');
 const session = require('express-session');
+const mongodbStore = require('connect-mongodb-session');
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
+const MongoDBStore = mongodbStore(session);
+
 const app = express();
+
+const sessionStore = new MongoDBStore({
+  uri: 'mongodb://127.0.0.1:27017',
+  databaseName: "auth-demo",
+  collection: "sessions",
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,12 +23,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  secret: 'super-secret',
-  resave: false,
-  saveUninitialized: false,
-  store: 
-}));
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
 
 app.use(demoRoutes);
 
