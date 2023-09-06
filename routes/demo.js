5dbca807ff0fa085034f9a2f69ff84769e61b950
beataj <beata@jasinski.com>
2023-09-06 16:07:59 -0700

    add data to session

--- routes/demo.js
+++ routes/demo.js
@@ -10,7 +10,18 @@ router.get("/", function (req, res) {
 });
 
 router.get("/signup", function (req, res) {
-  res.render("signup");
+  let sessionInputData = req.session.inputData;
+
+  if(!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: '',
+      confirmEmail: '',
+      password: ''
+    }
+  }
+
+  res.render("signup", {inputData: sessionInputData});
 });
 
 router.get("/login", function (req, res) {
@@ -31,8 +42,19 @@ router.post("/signup", async function (req, res) {
     enteredEmail !== enteredConfirmEmail ||
     !enteredEmail.includes("@")
   ) {
-    console.log("Incorrect data");
-    return res.redirect("/signup");
+   
+    req.session.inputData = {
+      hasError: true,
+      message: 'Invalid input - please check your data.',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    }
+
+    req.session.save(() => {
+      res.redirect("/signup");
+    })
+    return;
   }
 
   const existingUser = await db
